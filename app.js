08c408a52ab07af1392f0cff801afe4abd4882bb
Victor Reis <139720688+VictorReis18@users.noreply.github.com>
2025-12-04 11:41:01 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,15 +6,9 @@ import {
     onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
-// ✅ TROQUE AQUI PELA SUA CONFIG
+
 const firebaseConfig = {
-      apiKey: "AIzaSyCfHezkpGYdgvt_6euD1drBZ4fb39aSNr8",
-  authDomain: "soldafacil-3ea8f.firebaseapp.com",
-  projectId: "soldafacil-3ea8f",
-  storageBucket: "soldafacil-3ea8f.firebasestorage.app",
-  messagingSenderId: "933275857351",
-  appId: "1:933275857351:web:341ed83cb8de3e626d29e2",
-  measurementId: "G-WL5YHJCBBJ"
+      //Dados do servidor
 };
 
 const app = initializeApp(firebaseConfig);
